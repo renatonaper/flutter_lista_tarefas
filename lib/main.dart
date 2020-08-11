4495574f6f61ce4a3114250ff543d367b288e2c5
Renato Naper <r.naper@spotpromo.com.br>
2020-08-11 19:29:15 -0300

    adicionando funcionalidade de remover item da lista

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,9 @@ class _HomeState extends State<Home> {
   List _toDoList = [];
   final _toDoController = TextEditingController();
 
+  Map<String, dynamic> _lastRemoved;
+  int _lastRemovedPos;
+
   @override
   void initState() {
     super.initState();
@@ -39,6 +42,23 @@ class _HomeState extends State<Home> {
      _toDoList.add(newTodo);
      _saveData();
    });
+  }
+
+  Future<Null> _refresh() async{
+    //para demorar um segundos
+    await Future.delayed(Duration(seconds: 1));
+
+    setState(() {
+      _toDoList.sort((a , b){
+        if (a["ok"] && !b["ok"]) return 1;
+        else if (!a["ok"] && b["ok"]) return -1;
+        else return 0;
+      });
+      _saveData();
+    });
+
+    return null;
+
   }
 
   @override
@@ -76,28 +96,72 @@ class _HomeState extends State<Home> {
             ),
           ),
           Expanded(
-            child: ListView.builder(
-                padding: EdgeInsets.only(top: 10.0),
-                itemCount: _toDoList.length,
-                itemBuilder: (context, index){
-                  return CheckboxListTile(
-                    title: Text(_toDoList[index]["title"]),
-                    value: _toDoList[index]["ok"],
-                    secondary: CircleAvatar(
-                      child: Icon(_toDoList[index]["ok"]? Icons.check : Icons.error),),
-                    onChanged: (c){
-                      setState(() {
-                        _toDoList[index]["ok"] = c;
-                        _saveData();
-                      });
-                    },
-                  );
-                }),
+            child: RefreshIndicator(
+              onRefresh: _refresh,
+              child: ListView.builder(
+                  padding: EdgeInsets.only(top: 10.0),
+                  itemCount: _toDoList.length,
+                  itemBuilder: buildItem),
+            )
           )
         ],
       )
     );
   }
+
+  Widget buildItem (BuildContext context, int index){
+    return Dismissible(
+      key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
+      background: Container(
+        color: Colors.red,
+        child: Align(
+          alignment: Alignment(-0.9, 0.0),
+          child: Icon(Icons.delete,color: Colors.white,)
+        ),
+      ),
+      direction: DismissDirection.startToEnd,
+      child:  CheckboxListTile(
+          title: Text(_toDoList[index]["title"]),
+          value: _toDoList[index]["ok"],
+          secondary: CircleAvatar(
+            child: Icon(_toDoList[index]["ok"]? Icons.check : Icons.error),),
+          onChanged: (c){
+            setState(() {
+              _toDoList[index]["ok"] = c;
+              _saveData();
+            });
+          }),
+      onDismissed: (direction){
+        setState(() {
+          _lastRemoved = Map.from(_toDoList[index]);
+          _lastRemovedPos = index;
+          _toDoList.removeAt(index);
+          _saveData();
+
+          final snack = SnackBar(
+            content: Text("Tarefa \"${_lastRemoved["title"]}\" removida!"),
+            action: SnackBarAction(
+              label: "Desfazer",
+              onPressed: (){
+                setState(() {
+                  _toDoList.insert(_lastRemovedPos, _lastRemoved);
+                  _saveData();
+                });
+              },
+            ),
+            duration: Duration(seconds: 2),
+          );
+
+          Scaffold.of(context).removeCurrentSnackBar();    // ADICIONE ESTE COMANDO
+          Scaffold.of(context).showSnackBar(snack);
+        });
+      },
+    );
+  }
+
+   /*
+
+   * */
 
   //tudo que utilizamos ac
   Future<File> _getFile() async{
